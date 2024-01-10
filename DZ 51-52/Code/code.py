64c96960b1475a8a00b17f8823ae4d433bfb2c3a
Yuri <yuri.kudynskij@gmail.com>
2024-01-10 19:36:10 +0200

    Add new functional to work 52

--- DZ 51-52/Code/code.py
+++ DZ 51-52/Code/code.py
@@ -1,4 +1,8 @@
+import json
+import os.path
+
 import requests
+
 
 class Money:
 
@@ -7,12 +11,22 @@ class Money:
         self.currency = currency
 
     def __str__(self):
-        return f"{self.amount} - {self.currency}"
+        return f"{self.amount:.2f} - {self.currency}"
 
 
 class BankAccount:
     accounts = []
-    __exchange_rate = []
+    __exchange_rate = {}
+
+    @classmethod
+    def get_exchange_rate(cls):
+        url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+        r = requests.get(url)
+        data = r.json()
+        for item in data:
+            currency = item["cc"]
+            rate = item["rate"]
+            cls.__exchange_rate[currency] = rate
 
     def __init__(self, owner_name, account_number, balance, currency):
         self.owner_name = owner_name
@@ -28,12 +42,12 @@ class BankAccount:
 
     def deposit(self, amount):
         self._balance.amount += amount
-        print(f"До рахунку {self.__account_number} додано {amount}")
+        print(f"До рахунку {self.__account_number} додано {amount:.2f} {self._balance.currency}")
 
     def withdraw(self, amount):
         if amount < self._balance.amount:
             self._balance.amount -= amount
-            print(f"З рахунку {self.__account_number} знято {amount}")
+            print(f"З рахунку {self.__account_number} знято {amount:.2f} {self._balance.currency} ")
         else:
             print(f"Не достатньо коштів на рахунку {self.__account_number}")
 
@@ -46,16 +60,24 @@ class BankAccount:
               f"\nAccount number: {self.__account_number}"
               f"\nBalance: {self._balance.amount} {self._balance.currency}")
 
-    def transfer(self, another_acc, amount):
-        if self._balance.currency != another_acc._balance.currency:
-            print("\nУ рахунків різні валюти!")
-        else:
-            if amount < self._balance.amount:
-                self._balance.amount -= amount
-                another_acc._balance.amount += amount
-                print(f"Кошти переслані з рахунку {self.__account_number} на {another_acc}")
+    def transfer_funds(self, target_account, amount):
+        self_currency = self._balance.currency
+        target_currency = target_account._balance.currency
+
+        try:
+            exchange_rate_to_target = BankAccount.__exchange_rate.get(target_currency, 1)
+            amount_in_target_currency = amount / exchange_rate_to_target
+
+            if self._balance.amount >= amount:
+                self.withdraw(amount)
+                target_account.deposit(amount_in_target_currency)
+                print(f"Трансфер {amount} {self_currency} відбувся.\n"
+                      f"Баланс рахунку {self.__account_number}: {self._balance}\n"
+                      f"Баланс рахунку {target_account.__account_number}: {target_account._balance}")
             else:
-                print(f"Не достатньо коштів на рахунку {self.__account_number}")
+                print(f"Недостатньо коштів на рахунку {self.__account_number} для трансферу.")
+        except Exception as e:
+            print(f"Помилка при обробці трансферу: {e}")
 
     @staticmethod
     def check_account_number(account_number):
@@ -85,3 +107,42 @@ class BankAccount:
         average = total / len(cls.accounts)
         return average
 
+    def save_data(self):
+        account_filename = os.path.join(os.path.dirname(__file__), "data", f"{self.__account_number}.txt")
+        account_data = {
+            "owner_name": self.owner_name,
+            "balance_amount": self._balance.amount,
+            "balance_currency": self._balance.currency
+        }
+
+        with open(account_filename, "w") as file:
+            json.dump(account_data, file)
+
+    def load_data(self):
+        account_name = os.path.join(os.path.dirname(__file__), "data", f"{self.__account_number}.txt")
+
+        if os.path.exists(account_name):
+            with open(account_name, "r") as file:
+                account_data = json.load(file)
+                self.owner_name = account_data["owner_name"]
+                self._balance.amount = account_data["balance_amount"]
+                self._balance.currency = account_data["balance_currency"]
+        else:
+            print("Файл не знайдено!")
+
+    @classmethod
+    def remove_account(cls, account_number):
+        for account in cls.accounts:
+            if account.__account_number == account_number:
+                cls.accounts.remove(account)
+
+                # Видалення файлу з даними
+                filename = os.path.join(os.path.dirname(__file__), "data", f"{account_number}.txt")
+                if os.path.exists(filename):
+                    os.remove(filename)
+
+                print(f"Рахунок {account_number} видалено.")
+
+                break
+        else:
+            print(f"Рахунок з номером {account_number} не знайдено.")
--- DZ 51-52/Code/test_code.py
+++ DZ 51-52/Code/test_code.py
@@ -1,16 +1,34 @@
-from code import BankAccount, Money
+from code import BankAccount
 
-user1 = BankAccount("Mikky Ruk", "31567", 200, "USD")
-user2 = BankAccount("Alice Parker", "34565", 100, "USD")
-user3 = BankAccount("Nikky Mick", "62162", 500, "EUR")
+user1 = BankAccount("Mikky Ruk", "31567", 1300, "UAH")
+user2 = BankAccount("Alice Parker", "34565", 1000, "USD")
+user3 = BankAccount("Nikky Mick", "62162", 800, "EUR")
+user4 = BankAccount("Alicon New", "21352", 1000, "JPY")
+user4.save_data()
+user4.remove_account("21352")
+print(*BankAccount.accounts)
+
+user1.save_data()
+user2.save_data()
+user3.save_data()
+
+user1.deposit(500)
+user1.save_data()
+
+user1.load_data()
+user1.display_account_info()
+
+
 
 user1.change_owner_name("Matiaz Kintry")
-
-user1.deposit(20)
-
-user1.transfer(user2, 100)
-user1.transfer(user3, 1)
-
+print()
+user1.transfer_funds(user2, 500)
+print()
+user2.transfer_funds(user3, 50)
+print()
+user3.transfer_funds(user1, 20)
+print()
+user1.transfer_funds(user3, 20000)
 print()
 
 user1.display_account_info()
@@ -31,3 +49,6 @@ average_balance = BankAccount.get_average_balance()
 print(f"Середнє значення всіх балансів {average_balance:.2f}")
 print()
 print(*BankAccount.accounts)
+
+print(BankAccount.check_account_number(user1.account_number))
+
